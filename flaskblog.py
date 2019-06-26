7ba56b6f36f7ca1fb983b9898deda6e0c9e1e6fc
Numan Khan <numankhan@Numans-MBP.home>
2019-06-25 21:47:04 -0400

    using jinja for looping through blog posts

--- flaskblog.py
+++ flaskblog.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author' : 'Numan Khan',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted' : 'June 25, 2019'
+    },
+    {
+        'author' : 'Aiza Khan',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted' : 'June 25, 2019'
+    },
+]
+
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
