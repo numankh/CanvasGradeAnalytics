a26297f2959c7c63a1c2494abf50f6f7f1a16edc
numankh <khannuman15@gmail.com>
2019-07-01 20:37:40 -0400

    Created html file for register page

--- flaskblog.py
+++ flaskblog.py
@@ -29,5 +29,15 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
+
 if __name__ == '__main__':
     app.run(debug=True)
