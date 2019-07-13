378e706a8e3b36d935db62dfe4b9b078566957be
numankh <khannuman15@gmail.com>
2019-07-13 12:50:50 -0400

    added flash msgs for login

--- flaskblog.py
+++ flaskblog.py
@@ -38,9 +38,16 @@ def register():
         return redirect(url_for('home'))    # redirecting to home
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        # Fake data for successful login (hardcoding)
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password.', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
